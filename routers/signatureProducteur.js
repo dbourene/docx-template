c070912d95fe54b26262a5c8ba38fc4dcf866d0e
dbourene <dbourene@audencia.com>
2025-07-30 06:38:20 +0200

    Commentaire ajouté

--- routers/signatureProducteur.js
+++ routers/signatureProducteur.js
@@ -1,4 +1,7 @@
 // Définit le endpoint POST /signature-producteur
+
+// import handleSignatureProducteur from '../services/handleSignatureProducteur.js';
+// router.post('/', handleSignatureProducteur);
 import express from 'express';
 import { handleSignatureProducteur } from '../services/contrat/handleSignatureProducteur.js';
 
